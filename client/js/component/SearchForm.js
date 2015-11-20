b876d2ea59f523a381fc1c541e9c4183f9396a28
pastak <pasta0915@gmail.com>
2015-11-21 04:21:40 +0900

    env

--- client/js/component/SearchForm.js
+++ client/js/component/SearchForm.js
@@ -41,22 +41,21 @@ export default class extends React.Component {
             </div>
           </form>
         </div>
-        {
-          (process.env.NODE_ENV === 'production')
-          ? [(<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>),
-              (<ins class="adsbygoogle"
-               style="display:block"
-               data-ad-client="ca-pub-4787283240527546"
-               data-ad-slot="6939564609"
-               data-ad-format="auto">
-              </ins>),
-              (<script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-              </script>)]
-          : null
-        }
         <div className='col-sm-2' />
       </div>
+      {(document.documentElement.dataset.env === 'production')
+        ? (<div className='ad'>
+            <ins className="adsbygoogle"
+               style={{display:'block'}}
+               data-ad-client='ca-pub-4787283240527546'
+               data-ad-slot='6939564609'
+               data-ad-format='auto'>
+            </ins>
+            <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+          </div>)
+        : null}
       <hr />
       <RecentKeywords />
       <hr />
